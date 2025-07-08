5de71a020daffad1b6daaa95de8fb5972f93b037
haixi1818 <haixiz2018@gmail.com>
2025-07-08 14:46:46 -0500

    SCRUM-71
    SCRUM-71: Add Config Server initial setup

---
+++ src/test/java/com/beaconfire/SCRUM_71_config_server/Scrum71ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaconfire.SCRUM_71_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scrum71ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
